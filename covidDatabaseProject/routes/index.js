7d6c5a9acc7bd186294d465ca447878b8cbb9891
Taejoon <taejoon.park@gatech.edu>
2020-11-23 17:16:48 -0500

    added screen4 and 6

--- covidDatabaseProject/routes/index.js
+++ covidDatabaseProject/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+// express.use(bodyParser.urlencoded({ extended: false }))
 
 const mysqlDb = require('./../mysqlConn')
 
@@ -100,21 +101,61 @@ router.get('/home_screen', (req, res, next) => {
 
 // Screen 4: Student View Test Results
 router.get('/view_my_results', (req, res, next) => {
-	res.render("screen4", {data:null})
+	console.log(req.cookies.username)
+	res.render("screen4", {result: [{
+		"test_id": null,
+		"timeslot_date": null,
+		"date_processed": null,
+		"pool_status": null,
+		"test_status": null
+	}] })
 })
 
-// router.get('/view_my_results_filtered', (req, res, next) => {
-	
-// 	mysqlDb.query('CALL student_view_results(?, null, ?, ?)',
-// 	[username, start_date, end_date],
-//   (error, results, fields) => {
-//    if (results.length > 0) {
-//      res.render('screen4', {data:results})
-//    } else {
-//      console.log("Error!");
-//    }		
-//  });
-// })
+router.post('/view_my_results_filtered', (req, res, next) => {
+	var username = req.cookies.username
+	var start_date = null
+	var end_date = null
+
+	if (req.body.start_date !== 'undefined'&& req.body.start_date) { 
+		start_date = req.body.start_date
+		start_date = String(start_date)
+	}
+
+	if (req.body.end_date !== 'undefined'&& req.body.end_date) { 
+		end_date = req.body.end_date
+		end_date = String(end_date)
+	}
+
+	console.log(username)
+	console.log(start_date)
+	console.log(end_date)
+
+	mysqlDb.query('CALL student_view_results(?, null, ?, ?)',
+		[username, start_date, end_date],
+		(error, results, fields) => {
+		});
+
+
+
+	mysqlDb.query(
+		'SELECT test_id, \
+		DATE_FORMAT(timeslot_date, "%m/%d/%Y") as timeslot_date, \
+		DATE_FORMAT(date_processed, "%m/%d/%Y") as date_processed, \
+		pool_status, test_status from student_view_results_result',
+		(error, results, fields) => {
+			if (results.length > 0) {
+				res.render('screen4', {result:results})
+			} else {
+				res.render("screen4", {result: [{
+					"test_id": "N/A",
+					"timeslot_date": "N/A",
+					"date_processed": "N/A",
+					"pool_status": "N/A",
+					"test_status": "N/A"
+				}] })
+		}
+	});	
+})
 
 // Screen 5: Explore Test Result
 router.get('/explore_test_result', (req, res, next) => {
@@ -123,8 +164,67 @@ router.get('/explore_test_result', (req, res, next) => {
 
 // Screen 6: Aggregate Test Results
 router.get('/view_aggregate_results', (req, res, next) => {
-	res.send("TODO")
+	console.log(req.cookies.username)
+
+	var dummy = [{
+		"total": "N/A",
+		"percent": "N/A"
+	}]
+	
+	mysqlDb.query('SELECT DISTINCT housing_type FROM student',
+	(error, results, fields) => {
+		if (results.length > 0) {
+			var housing_type = results;
+			mysqlDb.query('SELECT DISTINCT location FROM student',
+				(error, results, fields) => {
+					if (results.length > 0) {
+						var location = results;
+						mysqlDb.query('select distinct site_name from site',
+						(error, results, fields) => {
+							if (results.length > 0) {
+									var sites = results;
+									res.render('screen6', {loc:location, housing:housing_type, sites:sites, data:dummy})
+								} else {
+									console.log("Error!");
+								}
+						});
+					} else {
+						console.log("Error!");
+					}
+			});
+		} else {
+			console.log("Error!");
+		}
+   });
 })
+
+router.post('/view_aggregate_results_filtered', (req, res, next) => {
+	mysqlDb.query('CALL aggregate_results(?, ?, ?, ?, ?)',
+		[location, housing_type, testing_site, start_date, end_date],
+		(error, results, fields) => {
+		});
+
+	mysqlDb.query(
+		'SELECT test_id, \
+		DATE_FORMAT(timeslot_date, "%m/%d/%Y") as timeslot_date, \
+		DATE_FORMAT(date_processed, "%m/%d/%Y") as date_processed, \
+		pool_status, test_status from student_view_results_result',
+		(error, results, fields) => {
+			if (results.length > 0) {
+				res.render('screen4', {result:results})
+			} else {
+				res.render("screen4", {result: [{
+					"test_id": "N/A",
+					"timeslot_date": "N/A",
+					"date_processed": "N/A",
+					"pool_status": "N/A",
+					"test_status": "N/A"
+				}] })
+		}
+	});	
+	
+})
+
 
 // Screen 7: Signup for a Test
 router.get('/sign_up_for_a_test', (req, res, next) => {
@@ -188,11 +288,12 @@ router.get('/view_daily_results', (req, res, next) => {
 
 // Debugging Screen
 router.get('/debug', (req, res, next) => {
-  mysqlDb.query('SELECT * FROM user',
+  mysqlDb.query('SELECT * FROM student',
   (error, results, fields) => {
    if (results.length > 0) {
-     res.render('debug', {title: "debug only", result:results})
-    //  res.json(results);
+    //  res.render('debug', {title: "debug only", result:results})
+	//  res.json(results);
+	res.send()
    } else {
      console.log("Error!");
    }		
